14e26419e0dd461b57a077f5ae1ca10ef3223a56
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-04-24 22:13:18 -0500

    [add] agregando interactividad con un CLI

--- encoder.py
+++ encoder.py
@@ -1,5 +1,6 @@
 import qrcode
 from PIL import Image
+import argparse
 
 def generate_qr_code(data: str, file_name: str) -> None:
     qr = qrcode.QRCode(
@@ -14,8 +15,16 @@ def generate_qr_code(data: str, file_name: str) -> None:
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(file_name)
 
+def main():
+    parser = argparse.ArgumentParser(description="Genera un código QR para texto, imágenes, videos o audio.")
+    parser.add_argument("format", choices=["texto", "imagen", "video", "audio"], help="El formato del contenido a codificar.")
+    parser.add_argument("data", help="El contenido a codificar o la URL del archivo multimedia.")
+    parser.add_argument("file_name", default="qr_code.png", nargs="?", help="El nombre del archivo donde se guardará el código QR.")
+    
+    args = parser.parse_args()
+
+    generate_qr_code(args.data, args.file_name)
+    print(f"Código QR generado y guardado como {args.file_name}")
+
 if __name__ == "__main__":
-    data = input("Ingresa el texto para generar el código QR: ")
-    file_name = "qr_code.png"
-    generate_qr_code(data, file_name)
-    print(f"Código QR generado y guardado como {file_name}")
+    main()
